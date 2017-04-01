0606cc85048eb39add77dfd4185477c815e1fbc6
Christopher Hiller <boneskull@boneskull.com>
2017-04-01 11:41:04 -0700

    updates

--- README.md
+++ README.md
@@ -1,5 +1,38 @@
-# thermoskull
+# shtshow
+
+> Temperature & humidity sensor Thing using [ESP8266] + [MQTT] + [SHT30].  Built with [PlatformIO] and [Homie]
+
+## Hardware
+
+I'm using a [Wemos D1 Mini] and a [SHT30 Shield].  Any ESP8266 and any SHT3x *should* work.
+
+> Note: The SHT30 has a I2C address of `0x45`, whereas the factory setting is `0x44`.  You can change this value in the source.
+
+## Installation
+
+1. Wire up hardware
+1. Get [PlatformIO]
+1. Clone repo
+1. Use PlatformIO to flash device
+1. Optionally, create `data/homie/config.json` and use PlatformIO to upload to SPIFFS
+1. Follow [Homie's setup instructions](https://homie-esp8266.readme.io/docs/getting-started) 
 
 ## Notes
 
-- Edit and run `ota.js` to run an OTA update after building firmware. 
+- [Wemos' provided library](https://github.com/wemos/WEMOS_SHT3x_Arduino_Library) is hinky and **not recommended**.
+- This firmware uses [ClosedCube's library](https://github.com/closedcube/ClosedCube_SHT31D_Arduino) instead.
+- For the D1 Mini, I had best results with `WAKE_RF_DEFAULT` when entering deep sleep.
+- Likewise, I'm not sure clock stretching works very well on an ESP8266, so we're using the SHT30's "periodic" mode.
+
+## License
+
+Copyright 2017 [Christopher Hiller].  Licensed MIT.
+
+[Homie]: https://homie-esp8266.readme.io/
+[SHT30]: https://www.sensirion.com/en/environmental-sensors/humidity-sensors/digital-humidity-sensors-for-various-applications/
+[PlatformIO]: http://platformio.org/
+[ESP8266]: http://esp8266.net/
+[Wemos D1 Mini]: https://www.wemos.cc/product/d1-mini.html
+[SHT30 Shield]: https://www.wemos.cc/product/sht30-shield.html
+[MQTT]: http://mqtt.org
+[Christopher Hiller]: https://boneskull.com
--- src/shtshow.cpp
+++ src/shtshow.cpp
@@ -1,15 +1,17 @@
 #include <Arduino.h>
 #include <Homie.h>
-#include <WEMOS_SHT3X.h>
+#include <Wire.h>
+#include <ClosedCube_SHT31D.h>
 
 #pragma clang diagnostic push
 #pragma ide diagnostic ignored "OCUnusedGlobalDeclarationInspection"
 #define DEFAULT_INTERVAL 60000 // ms
 #define DEFAULT_FAHRENHEIT_FLAG true
+#define I2C_ADDRESS 0x45
 
 using namespace std;
 
-SHT3X sht30(0x45);
+ClosedCube_SHT31D sht30;
 
 HomieNode tempNode("temperature", "environment");
 HomieNode humidityNode("humidity", "environment");
@@ -22,30 +24,48 @@ HomieSetting<bool> fahrenheitSetting("use_fahrenheit",
 void onHomieEvent (const HomieEvent &event) {
   switch (event.type) {
     case HomieEventType::READY_TO_SLEEP:
-      Homie.getLogger() << "Sleeping for " << intervalSetting.get()
-        << " ms" << endl;
-      ESP.deepSleep(intervalSetting.get() * 1e3, WAKE_RF_DEFAULT);
+      Homie.getLogger() << "Sleeping for " << intervalSetting.get() << " ms"
+        << endl;
+      ESP.deepSleep((uint32_t) (intervalSetting.get() * 1e3), WAKE_RF_DEFAULT);
       break;
     default:
       break;
   }
 }
 
+float celsiusToFahrenheit (float c) {
+  return (c * 9 / 5) + 32;
+}
+
 void setup () {
   Serial.begin(115200);
+  sht30.begin(I2C_ADDRESS);
+
   Homie_setFirmware("shtshow", "0.1.0");
 
   // these two handlers start "doing actual stuff" if Homie boots in "normal"
   // mode, which is why the stuff they do is not in this function.
   Homie.setSetupFunction(function<void ()>([] () {
-    sht30.get();
-    float tempF = sht30.fTemp;
-    float humidity = sht30.humidity;
-    Serial << "temp: " << tempF << ", humidity: " << humidity << endl;
-    tempNode.setProperty("unit").send(fahrenheitSetting.get() ? "F" : "C");
+
+    SHT31D result = sht30.readTempAndHumidity(REPEATABILITY_HIGH,
+      MODE_POLLING, 50);
+
+    float temp = result.t;
+    float rh = result.rh;
+
+    bool useFahrenheit = fahrenheitSetting.get();
+    if (useFahrenheit) {
+      temp = celsiusToFahrenheit(temp);
+    }
+
+    Serial << "temp: " << temp << ", humidity: " << rh << endl;
+
+    tempNode.setProperty("unit").send(useFahrenheit ? "F" : "C");
+    tempNode.setProperty("degrees").send(String(temp));
+
     humidityNode.setProperty("unit").send("RH");
-    tempNode.setProperty("degrees").send(String(tempF));
-    humidityNode.setProperty("percentage").send(String(humidity));
+    humidityNode.setProperty("percentage").send(String(rh));
+
     Homie.prepareToSleep();
   })).onEvent(onHomieEvent);
 
@@ -53,7 +73,6 @@ void setup () {
     function<bool (long)>([] (long value) {
       return value >= 2000;
     }));
-
   fahrenheitSetting.setDefaultValue(DEFAULT_FAHRENHEIT_FLAG);
 
   humidityNode.advertise("unit");
